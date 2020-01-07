21770e6dfc30d784e570439611dea2a80ef6826c
Forhadur Rahman <59611130+Forhad1989@users.noreply.github.com>
2020-01-08 01:46:13 +0600

    form fr pc
    form fr pc

--- Jaguar/index.php
+++ Jaguar/index.php
@@ -1,4 +1,26 @@
 <?php
+// $shohan   = array(array("Forhad","FR","RXD"),
+//                   array("10","11","12"),
+//                   array("20","21","22"),
+//
+//                 ); //array declar
+//
+//
+//
+//
+//  $i = 0;
+// $arrcount = count($shohan);
+// $end = $arrcount-1;
+//
+//   for ($i; $i<=$end; $i++){
+//     for($J=0; $J<=$end; $J++){
+//
+//     echo "<tr>";
+//     echo $shohan[$i][$J];
+//     echo "<tr>";
+// }
+//   }
+             //variable declare
 
 // $shohan = 10; //variable declare
 // $shohan = array(5,10, 7,13,15,45,54,"hello"); // array declare
@@ -22,18 +44,33 @@
 //   echo "<br>";
 // }
 
-$start= 1;
-// // // for($start; $start <=10; $start++){
-// // //   echo $start."<br>";
-// // }
-// // while($start <= 10){
-// //   echo $start."<br>";
-// //   $start++;
-//
-// }
 
-do {
-  echo $start. "<br>"; 
-}while ($start <=  10);
+// $start= 1;
+// // // // for($start; $start <=10; $start++){
+// // // //   echo $start."<br>";
+// // // }
+// // // while($start <= 10){
+// // //   echo $start."<br>";
+// // //   $start++;
+// // }
+
+// do {
+//   echo $start. "<br>";
+//   $start++;
+// }while ($start <= 10)
+
+// $number_one = 10 ;
+// echo ++$number_one; //pre incriment
+// echo $number_one++; //post incriment
+
+// echo "\$ean"; // $ print
+
+// $number = "5";
+// if ($number === 5) {
+//   echo "This is five";
+// }
+// else {
+//   echo "This not five";
+// }
 
 ?>
